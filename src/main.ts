45ac231de237a3346b0b3432253fea5f9d077421
Abdel Rahman Salem <99850666+Salemabdelrahman@users.noreply.github.com>
2025-12-18 13:35:00 +0200

    chore: commit user changes and add GH Actions workflow

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
+import { provideZoneChangeDetection } from "@angular/core";
 import { platformBrowser } from '@angular/platform-browser';
 import { AppModule } from './app/app-module';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
-platformBrowser().bootstrapModule(AppModule, {
-  ngZoneEventCoalescing: true,
-})
+platformBrowser().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection({ eventCoalescing: true })], })
   .catch(err => console.error(err));
